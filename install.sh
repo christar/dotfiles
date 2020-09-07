f9e35639eeaec724a3837c6b647857cd7f1b098f
christar <christar@users.noreply.github.com>
2020-09-07 23:42:55 +0200

    add backup commands to be safe

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 # tmux
-ln -s $PWD/tmux.conf ~/.tmux.conf
+## back up existing config first
+cp -f "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak" 2>/dev/null || true
+ln -sf $PWD/tmux.conf ~/.tmux.conf
 
 # vim/neovim
-ln -s $PWD/vimrc ~/.vim/
+## back up existing config first
+cp -f "$HOME/.vimrc" "$HOME/.vimrc.bak" 2>/dev/null || true
+cp -f "$HOME/.vim/vimrc" "$HOME/.vim/vimrc.bak" 2>/dev/null || true
+ln -sf $PWD/vimrc ~/.vim/vimrc
