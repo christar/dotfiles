32ef65e3b4dc51edb66c058fea1626781cd71121
christar <christar@users.noreply.github.com>
2020-09-08 00:27:40 +0200

    re-organize the dotfiles

--- install.sh
+++ install.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
+# oh-my-zsh
+if [ -d "$HOME/.oh-my-zsh/custom" ]; then 
+    cp -f $PWD/oh-my-zsh/my-custom.zsh $HOME/.oh-my-zsh/custom/
+else
+    echo "oh-my-zsh custom folder is not found!" 
+    echo "check if oh-my-zsh is installed!"
+fi
+
+
 # tmux
 ## back up existing config first
 cp -f "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak" 2>/dev/null || true
-ln -sf $PWD/tmux.conf ~/.tmux.conf
+ln -sf $PWD/tmux/tmux.conf ~/.tmux.conf
+
 
 # vim/neovim
 ## back up existing config first
 cp -f "$HOME/.vimrc" "$HOME/.vimrc.bak" 2>/dev/null || true
 cp -f "$HOME/.vim/vimrc" "$HOME/.vim/vimrc.bak" 2>/dev/null || true
-ln -sf $PWD/vimrc ~/.vim/vimrc
+ln -sf $PWD/vim/vimrc ~/.vim/vimrc
+
---
+++ oh-my-zsh/my-custom.zsh
@@ -0,0 +1,6 @@
+## alias
+
+alias bug="brew update && brew upgrade"
+alias n="nvim -u ~/.vim/vimrc"
+alias t="open -a typora"
+
