4114475efd8f457820849ad47cfb934307946d85
Yan <yan@Yan-Desktop.fritz.box>
2021-08-06 00:11:43 +0200

    add custom config for zprezto

--- install.sh
+++ install.sh
@@ -6,8 +6,16 @@ if [ -d "$HOME/.oh-my-zsh/custom" ]; then
 else
     echo "oh-my-zsh custom folder is not found!"
     echo "check if oh-my-zsh is installed!"
+    echo "skipping oh-my-zsh configuration"
+    echo ""
 fi
 
+# prezto
+if [ -d "$HOME/.zprezto/runcoms" ]; then
+    cat $PWD/oh-my-zsh/my-custom.zsh >> $HOME/.zprezto/runcoms/zshrc
+    echo "appended my-custom.zsh to zprezto zshrc"
+    echo ""
+fi
 
 # tmux
 ## back up existing config first
@@ -17,8 +25,12 @@ ln -sf $PWD/tmux/tmux.conf ~/.tmux.conf
 
 # vim/neovim
 ## back up existing config first
-cp -f "$HOME/.vimrc" "$HOME/.vimrc.bak" 2>/dev/null || true
-cp -f "$HOME/.vim/vimrc" "$HOME/.vim/vimrc.bak" 2>/dev/null || true
+if [ -d "$HOME/.vim" ]; then
+    cp -f "$HOME/.vimrc" "$HOME/.vimrc.bak" 2>/dev/null || true
+    cp -f "$HOME/.vim/vimrc" "$HOME/.vim/vimrc.bak" 2>/dev/null || true
+else
+    mkdir $HOME/.vim
+fi
 ln -sf $PWD/vim/vimrc ~/.vim/vimrc
 
 ## install vundle
